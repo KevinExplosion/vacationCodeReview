824927738ea2762197f29d826f77dc278b5fd578
Kevin <mattisonii@yahoo.com>
2016-01-15 14:37:36 -0800

    Fill in content for quiz questions and add glphyicons.

--- js/script.js
+++ js/script.js
@@ -1,22 +1,7 @@
 $(document).ready(function() {
   $("form#vacation").submit(function(event) {
 
-    var result = parseInt($(""))
-
-    if (result <= 9) {
-      //display moon
-    }
-
-    else if (result >= 20) {
-      //display hell
-    }
-
-    else (result === 15) {
-      //display dubai
-    };
-
-
-$("#result").show()
+    var desire = parseInt($(["input#answer1" + "input#answer2" + "input#answer3" + "input#answer4" "input#answer5"]))
 
 event.preventDefault();
 
