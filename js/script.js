ca97f3733403301204cd34d7693fe538c9048413
Kevin <mattisonii@yahoo.com>
2016-01-15 11:39:15 -0800

    Change input boxes to number wheels.

--- js/script.js
+++ js/script.js
@@ -1,8 +1,24 @@
 $(document).ready(function() {
+  $("form#vacation").submit(function(event) {
+
+    var result = parseInt($(""))
+
+    if (result <= 9) {
+      //display moon
+    }
+
+    else if (result >= 20) {
+      //display hell
+    }
+
+    else (result === 15) {
+      //display dubai
+    };
+
 
 $("#result").show()
 
 event.preventDefault();
 
-});
+  });
 });
