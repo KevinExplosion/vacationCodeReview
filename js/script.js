ee3e2c32124bb562dd84e22a476a6ef40c5d89f6
Kevin <mattisonii@yahoo.com>
2016-01-15 14:51:01 -0800

    Create shell of js file. Currently not working.

--- js/script.js
+++ js/script.js
@@ -1,7 +1,20 @@
 $(document).ready(function() {
   $("form#vacation").submit(function(event) {
 
-    var desire = parseInt($(["input#answer1" + "input#answer2" + "input#answer3" + "input#answer4" "input#answer5"]))
+    var ans1 = parseInt($("input#answer1").val());
+    var ans2 = parseInt($("input#answer2").val());
+    var ans3 = parseInt($("input#answer3").val());
+    var ans4 = parseInt($("input#answer4").val());
+    var ans5 = parseInt($("input#answer5").val());
+
+    var desire = (ans1 + ans2 + ans3 + ans4 + ans5);
+    if (desire <= 10) {
+      $(".moon").show();
+    } else if (desire <= 20) {
+      $(".dubai").show();
+    } else {
+      $(".heck").show();
+    };
 
 event.preventDefault();
 
