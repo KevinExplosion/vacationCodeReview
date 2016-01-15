416726ce256b5c1dd5afcceffa9198def5572f3f
Kevin <mattisonii@yahoo.com>
2016-01-15 15:38:07 -0800

    Add js to retrieve values from select boxes.

--- js/script.js
+++ js/script.js
@@ -1,16 +1,16 @@
 $(document).ready(function() {
-  $("form#vacation").submit(function(event) {
+  $("#vacation").submit(function(event) {
 
-    var ans1 = parseInt($("input#answer1").val());
-    var ans2 = parseInt($("input#answer2").val());
-    var ans3 = parseInt($("input#answer3").val());
-    var ans4 = parseInt($("input#answer4").val());
-    var ans5 = parseInt($("input#answer5").val());
+    var ans1 = parseInt($("select#answer1").val());
+    var ans2 = parseInt($("select#answer2").val());
+    var ans3 = parseInt($("select#answer3").val());
+    var ans4 = parseInt($("select#answer4").val());
+    var ans5 = parseInt($("select#answer5").val());
 
     var desire = (ans1 + ans2 + ans3 + ans4 + ans5);
-    if (desire <= 10) {
+    if (desire <= 6) {
       $(".moon").show();
-    } else if (desire <= 20) {
+    } else if (desire <= 19) {
       $(".dubai").show();
     } else {
       $(".heck").show();
